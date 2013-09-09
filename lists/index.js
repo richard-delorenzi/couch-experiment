e2661ad5acc199208edcc851ab463646d7f1935f
richard.delorenzi <none@none>
2013-09-09 17:28:13 +0100

    refactor

--- lists/index.js
+++ lists/index.js
@@ -11,24 +11,32 @@ function(head, req) {
     // The provides function serves the format the client requests.
     // The first matching format is sent, so reordering functions changes 
     // thier priority. In this case HTML is the preferred format, so it comes first.
+
+    function addHas(stash)
+    {
+	return stash;
+    }
+
     function stash()
     {
 	var key = "";
 	// render the html head using a template
 	var stash = {
-	    rides : List.withRows(function(row) {
-		var ride = row.value;
-		key = row.key;
-		return {
-		    has_wait_time_min : ride.wait_time_min ? true : false,
-                    wait_time_min: ride.wait_time_min,
-
-		    has_description: ride.description ? true : false,
-		    description: ride.description,
-
-		    name : ride.name
-		};
-	    })
+	    rides : List.withRows(
+		function(row) {
+		    var ride = row.value;
+		    key = row.key;
+		    var ride_stash = {
+			has_wait_time_min : ride.wait_time_min ? true : false,
+			wait_time_min: ride.wait_time_min,
+			
+			has_description: ride.description ? true : false,
+			description: ride.description,
+			
+			name : ride.name
+		    };
+		    return ride_stash;
+		})
 	};
 	return stash;
     }
