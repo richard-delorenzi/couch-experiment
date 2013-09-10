c9e048245a94af94979acd9b0f719fb6f8a9f4f0
richard.delorenzi <none@none>
2013-09-10 10:21:42 +0100

    add fix to reservation|ride join: ensure in correct order

--- views/rides/map.js
+++ views/rides/map.js
@@ -1,8 +1,8 @@
 function(doc) {  
     if (doc.type == "ride") {
-	emit([doc._id], doc);
+	emit([doc._id,1], doc);
     }
     if (doc.type == "reservation" ) {
-	emit([doc.attraction_id], doc);
+	emit([doc.attraction_id,0], doc);
     }
 };
