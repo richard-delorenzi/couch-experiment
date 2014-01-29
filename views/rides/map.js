8ddf4a5477b0d89d23b1deb970397b73c52fff35
richard.delorenzi <none@none>
2014-01-29 15:07:25 +0000

    added ride status

--- views/rides/map.js
+++ views/rides/map.js
@@ -1,8 +1,12 @@
 function(doc) {  
-    if (doc.type == "ride") {
-	emit([doc._id,1], doc);
-    }
     if (doc.type == "reservation" ) {
 	emit([doc.attraction_id,0], doc);
     }
+    if (doc.type == "ride") {
+	emit([doc._id,1], doc);
+    }
+    if (doc.type == "ride-status" ) {
+	emit([doc.attraction_id,2], doc);
+    }
+   
 };
