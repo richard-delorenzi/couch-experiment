8ee1fb4297a354bc667d771d5cd4b4122f530416
richard.delorenzi <none@none>
2014-02-03 21:21:53 +0000

    add unit tests for library

--- test/unit-test/test-lib.js
+++ test/unit-test/test-lib.js
@@ -1,9 +1,53 @@
+test( "addIfdef", function() {
+    var stash = {
+	"o" : { 
+            "e":"E",
+            "f": { 
+		"g":"G" 
+            }
+	},
+	"z": 
+	[ "hello",
+          { 
+	      "a": "A", 
+	      "b": "B"
+          }
+        ]
+    };
 
-    
-test( "hello test", function() {
-    ok( 1 == "1", "Passed!" );
+    var expectedStash =  {
+	"ifdef_o" : true,
+	"ifdef_z" : true,
+	"o" : { 
+            "ifdef_e" : true,
+            "ifdef_f" : true,
+            "e":"E",
+            "f": { 
+		"ifdef_g" : true,
+		"g":"G" 
+            }
+	},
+	"z": 
+	[ "hello",
+          {
+	      "ifdef_a" : true,
+	      "ifdef_b" : true,
+	      "a": "A",
+	      "b": "B"
+          }
+        ]
+    };
+
+    var output = addIfdef(stash);
+    deepEqual( output, expectedStash);
 });
-    
-    
+
+test( "listify", function() {
+    deepEqual( listify("hello"), ["hello"] );
+    deepEqual( listify(["hello"]), ["hello"] );
+    deepEqual( listify(true), [true] );
+    deepEqual( listify(123), [123] );
+    deepEqual( listify(undefined), null);
+});
 
 
